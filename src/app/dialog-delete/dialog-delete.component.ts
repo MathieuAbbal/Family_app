3e79a5b59f94e0536bc6daf23aa9588b5ffee559
Mathieu Abbal <mathieuabbal@hotmail.com>
2022-01-16 08:30:28 +0100

    supprim task

--- src/app/dialog-delete/dialog-delete.component.ts
+++ src/app/dialog-delete/dialog-delete.component.ts
@@ -1,14 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { TasksService } from '../services/tasks.service';
+import { Task } from '../models/task.model';
 
 @Component({
   selector: 'app-dialog-delete',
   templateUrl: './dialog-delete.component.html',
   styleUrls: ['./dialog-delete.component.css']
 })
+
 export class DialogDeleteComponent implements OnInit {
+  @Input()item:any
+  constructor(private ts :TasksService,
+    
+    ) { }
+  tasks: Task[] = [];
+  tasksSubsription!: Subscription;
 
-  constructor() { }
-
+  onDeleteTask(task:Task) {
+    this.ts.removeTask(task);
+  }
+ 
+  ngOnDestroy(){
+    this.tasksSubsription.unsubscribe();
+    
+  }
   ngOnInit(): void {
   }
 
