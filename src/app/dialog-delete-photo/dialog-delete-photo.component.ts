4d0c6c5e1bbad8a79c60cf2fd5342ecd4b7d0c01
Mathieu Abbal <mathieuabbal@hotmail.com>
2022-02-05 15:44:49 +0100

    ajout dialog delete photo

--- src/app/dialog-delete-photo/dialog-delete-photo.component.ts
+++ src/app/dialog-delete-photo/dialog-delete-photo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Photo } from '../models/photo.model';
 import { PhotosService } from '../services/photos.service';
@@ -6,23 +6,20 @@ import { PhotosService } from '../services/photos.service';
 @Component({
   selector: 'app-dialog-delete-photo',
   templateUrl: './dialog-delete-photo.component.html',
-  styleUrls: ['./dialog-delete-photo.component.css']
+  styleUrls: ['./dialog-delete-photo.component.css'],
 })
 export class DialogDeletePhotoComponent implements OnInit {
- @Output()item:any
-  constructor(
-    private ps :PhotosService,
-  ) { }
+  @Input() item: any;
+  constructor(private ps: PhotosService) {}
 
-    photos: Photo[]= [];
-    photosSubsription!:Subscription;
+  photos: Photo[] = [];
+  photosSubsription!: Subscription;
 
-
-
-    onDeletePhoto(){
-      
-    }
-  ngOnInit(): void {
+  onDeletePhoto(photo: Photo) {
+    this.ps.removePhoto(photo);
   }
-
+  ngOnInit(): void {}
+  ngOnDestroy() {
+    this.photosSubsription.unsubscribe();
+  }
 }
