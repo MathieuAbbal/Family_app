6602c7a35ce1fea51077b0134f410621758d0bdb
Mathieu Abbal <mathieuabbal@hotmail.com>
2022-03-27 09:55:29 +0200

    ajout composant map

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AddTaskComponent } from './add-task/add-task.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { HomeComponent } from './home/home.component';
+import { MapComponent } from './map/map.component';
 import { PhotoComponent } from './photo/photo.component';
 import { AuthGuardService } from './services/auth-guard.service';
 import { ShoppingComponent } from './shopping/shopping.component';
@@ -16,6 +17,7 @@ const routes: Routes = [
   { path: 'add-task', canActivate: [AuthGuardService],   component: AddTaskComponent },
   { path: 'photo', canActivate: [AuthGuardService],  component: PhotoComponent },
   { path: 'shopping', canActivate: [AuthGuardService],  component: ShoppingComponent },
+  { path: 'map', canActivate: [AuthGuardService],  component: MapComponent },
 ];
 
 @NgModule({
