655d17a83d1a138810bd842c856ba0e4f3a3d36d
Mathieu Abbal <mathieuabbal@hotmail.com>
2022-09-23 17:47:24 +0200

    correctif leaflet style bug

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, AfterViewInit, NgZone } from '@angular/core';
+import * as L from 'leaflet';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent  {
+export class MapComponent implements AfterViewInit {
 
-  constructor() { }
+  constructor(private zone: NgZone) { }
 
-  
- 
+
+
+
+  ngAfterViewInit() {
+
+
+    const map = L.map('map').setView([50.6311634, 3.0599573], 12);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: 'map'
+    }).addTo(map);
+  }
 }
+
