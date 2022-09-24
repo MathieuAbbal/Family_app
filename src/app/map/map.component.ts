ec52b41c005bcac5d74154c5dffab285b78b4d73
Mathieu Abbal <mathieuabbal@hotmail.com>
2022-09-24 09:53:44 +0200

    localisation map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit, NgZone } from '@angular/core';
 import * as L from 'leaflet';
+import { Observable, Subscriber } from 'rxjs';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -7,19 +8,48 @@ import * as L from 'leaflet';
 })
 export class MapComponent implements AfterViewInit {
 
+  map: any;
+
+
+
   constructor(private zone: NgZone) { }
 
-
-
-
-  ngAfterViewInit() {
-
-
-    const map = L.map('map').setView([50.6311634, 3.0599573], 12);
-
+  private getCurrentPosition(): any {
+    return new Observable((observer: Subscriber<any>) => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position: any) => {
+          observer.next({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+          observer.complete();
+        });
+      } else {
+        observer.error();
+      }
+    });
+  }
+  private loadMap(): void {
+    this.map = L.map('map').setView([0, 0], 1);
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: 'map'
-    }).addTo(map);
+    }).addTo(this.map);
+    this.getCurrentPosition()
+    .subscribe((position: any) => {
+      this.map.flyTo([position.latitude, position.longitude], 13);
+      const icon = L.icon({
+        iconUrl: 'https://res.cloudinary.com/rodrigokamada/image/upload/v1637581626/Blog/angular-leaflet/marker-icon.png',
+        shadowUrl: 'https://res.cloudinary.com/rodrigokamada/image/upload/v1637581626/Blog/angular-leaflet/marker-shadow.png',
+        popupAnchor: [13, 0],
+      });
+
+      const marker = L.marker([position.latitude, position.longitude], { icon }).bindPopup('Angular Leaflet');
+      marker.addTo(this.map);
+    });
+  }
+
+  ngAfterViewInit() {
+    this.loadMap()
   }
 }
 
