92b8779e5e43eeba8d2bb2d57c6925bd05ab8994
Mathieu Abbal <mathieuabbal@hotmail.com>
2023-11-19 09:02:42 +0100

    maplibre

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,5 @@
-import { Component, AfterViewInit, NgZone } from '@angular/core';
-import * as L from 'leaflet';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import maplibregl, { Map, NavigationControl } from 'maplibre-gl';
 import { Observable, Subscriber } from 'rxjs';
 @Component({
   selector: 'app-map',
@@ -12,8 +12,9 @@ export class MapComponent implements AfterViewInit {
 
 
 
-  constructor(private zone: NgZone) { }
-
+  constructor() { }
+  @ViewChild('map')
+  private mapContainer!: ElementRef<HTMLElement>;
   private getCurrentPosition(): any {
     return new Observable((observer: Subscriber<any>) => {
       if (navigator.geolocation) {
@@ -30,25 +31,26 @@ export class MapComponent implements AfterViewInit {
     });
   }
   private loadMap(): void {
-    this.map = L.map('map').setView([0, 0], 1);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: 'map'
-    }).addTo(this.map);
-    this.getCurrentPosition()
-    .subscribe((position: any) => {
-      this.map.flyTo([position.latitude, position.longitude], 13);
-      const icon = L.icon({
-        iconUrl: 'https://res.cloudinary.com/rodrigokamada/image/upload/v1637581626/Blog/angular-leaflet/marker-icon.png',
-        shadowUrl: 'https://res.cloudinary.com/rodrigokamada/image/upload/v1637581626/Blog/angular-leaflet/marker-shadow.png',
-        popupAnchor: [13, 0],
-      });
+    const myAPIKey = '165c18e3661c43e9893446644364cbbb';
+    const mapStyle = 'https://maps.geoapify.com/v1/styles/osm-liberty/style.json';
 
-      const marker = L.marker([position.latitude, position.longitude], { icon }).bindPopup('Angular Leaflet');
-      marker.addTo(this.map);
+    const initialState = {
+      lng: 2.213749,
+      lat: 46.227638,
+      zoom: 5,
+    };
+
+    const map = new Map({
+      container: this.mapContainer.nativeElement,
+      style: `${mapStyle}?apiKey=${myAPIKey}`,
+      center: [initialState.lng, initialState.lat],
+      zoom: initialState.zoom,
     });
+    map.addControl(new NavigationControl());
   }
 
   ngAfterViewInit() {
+    this.getCurrentPosition()
     this.loadMap()
   }
 }
