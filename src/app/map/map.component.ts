ccf6a36bd40d1f88e20d4ea449320664597eac42
Mathieu Abbal <mathieuabbal@hotmail.com>
2023-12-05 20:48:32 +0100

    mapLibre 1.15.2

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,18 +1,39 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { NavigationControl,Map } from 'maplibre-gl';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent  {
-
-
-
-
+export class MapComponent implements AfterViewInit  {
 
   constructor() { }
-  
+  @ViewChild('map')
+  private mapContainer: ElementRef<HTMLElement>;
+
+  private map: Map;
+  ngAfterViewInit() {
+    // This API key is for use only in stackblitz.com
+    // Get your Geoapify API key on https://www.geoapify.com/get-started-with-maps-api
+    // The Geoapify service is free for small projects and the development phase.
+    const myAPIKey = '793f93202015411eaa6fceaeadaad99c';
+    const mapStyle = 'https://maps.geoapify.com/v1/styles/osm-carto/style.json';
+
+    const initialState = {
+      lng: 2.213749,
+      lat: 46.227638,
+      zoom: 5,
+    };
+
+    this.map = new Map({
+      container: this.mapContainer.nativeElement,
+      style: `${mapStyle}?apiKey=${myAPIKey}`,
+      center: [initialState.lng, initialState.lat],
+      zoom: initialState.zoom,
+    });
+
+  }
  
 }
 
--- src/app/user/user-profile/user-profile.component.ts
+++ src/app/user/user-profile/user-profile.component.ts
@@ -26,11 +26,6 @@ export class UserProfileComponent {
         this.user = userData;
         console.log('user', this.user);
         this.initForm(this.user)
-        if (this.user?.photoURL) {
-          this.fileUrl = this.user.photoURL;
-        } else {
-          this.fileUrl = 'https://static.vecteezy.com/system/resources/previews/026/966/960/non_2x/default-avatar-profile-icon-of-social-media-user-vector.jpg';
-        }
       },
       (error) => {
         console.error(error);
@@ -47,7 +42,7 @@ export class UserProfileComponent {
       dateBirthControl: [user?.dateBirth || ''],
     });
     this.fileUrl = this.user?.photoURL || '';
-
+  
   }
   loadFile(file: File): void {
     this.fileIsUploading = true;
@@ -84,7 +79,7 @@ export class UserProfileComponent {
     }
   }
   ngAfterViewInit() {
-
+    
   }
 }
 
