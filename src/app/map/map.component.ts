982c4accbfc55c4bc72142cc840f807a0bc1f4b1
Mathieu Abbal <mathieuabbal@hotmail.com>
2023-12-09 08:17:54 +0100

    map control

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,24 +1,42 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { NavigationControl,Map } from 'maplibre-gl';
+import maplibregl from 'maplibre-gl';
+import { NavigationControl, Map } from 'maplibre-gl';
+import { Observable, Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements AfterViewInit  {
+export class MapComponent implements AfterViewInit {
 
   constructor() { }
   @ViewChild('map')
   private mapContainer: ElementRef<HTMLElement>;
 
   private map: Map;
+
+  private getCurrentPosition(): any {
+    return new Observable((observer: Subscriber<any>) => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position: any) => {
+          observer.next({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+          observer.complete();
+        });
+      } else {
+        observer.error();
+      }
+    });
+  }
+
   ngAfterViewInit() {
-    // This API key is for use only in stackblitz.com
     // Get your Geoapify API key on https://www.geoapify.com/get-started-with-maps-api
     // The Geoapify service is free for small projects and the development phase.
     const myAPIKey = '793f93202015411eaa6fceaeadaad99c';
-    const mapStyle = 'https://maps.geoapify.com/v1/styles/osm-carto/style.json';
+    const mapStyle = 'https://maps.geoapify.com/v1/styles/maptiler-3d/style.json';
 
     const initialState = {
       lng: 2.213749,
@@ -33,7 +51,27 @@ export class MapComponent implements AfterViewInit  {
       zoom: initialState.zoom,
     });
 
+    this.map.addControl(new maplibregl.NavigationControl({
+      visualizePitch: true,
+      showZoom: true,
+      showCompass: true
+    }));
+    // Initialize the geolocate control.
+    let geolocate = new maplibregl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true,
+      showUserLocation: true
+    });
+    // Add the control to the map.
+    this.map.addControl(geolocate);
+    // when a geolocate event occurs.
+    geolocate.on('geolocate', function () {
+      console.log('A geolocate event has occurred.')
+    });
+    geolocate.trigger();
   }
- 
+
 }
 
