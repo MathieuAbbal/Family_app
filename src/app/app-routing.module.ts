c19dda4023c66612e08e1bcb5f0ed2e367f80def
Mathieu Abbal <mathieuabbal@hotmail.com>
2023-12-10 09:58:19 +0100

    mat-card home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
   { path: 'photo', canActivate: [AuthGuardService],  component: PhotoComponent },
   { path: 'shopping', canActivate: [AuthGuardService],  component: ShoppingComponent },
   { path: 'map', canActivate: [AuthGuardService],  component: MapComponent },
-  { path: 'edit/:index', canActivate: [AuthGuardService], component: EditTaskComponent },
+  { path: 'task/edit/:index', canActivate: [AuthGuardService], component: EditTaskComponent },
 ];
 
 @NgModule({
