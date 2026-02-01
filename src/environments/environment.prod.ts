00bad04f841fe3ddfbcdd036f1ba59ba8fe7c911
mathieuabbal <mathieuabbal@hotmail.com>
2026-02-01 16:05:45 +0100

    github pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   googleCalendar: {
-    apiKey: 'AIzaSyCWPkRbnK0mPYUuy1pGFMZV2QQ0MZVIQCU',
-    clientId: '178823203850-gf2l9715l6162j6k8lk8bhu2flobv03a.apps.googleusercontent.com',
+    apiKey: '***FIREBASE_API_KEY***',
+    clientId: '***GOOGLE_CLIENT_ID***',
     discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
     scopes: 'https://www.googleapis.com/auth/calendar',
     familyCalendarId: '***FAMILY_CALENDAR_ID***'
