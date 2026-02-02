ecea7afc97457d5e077ef363039f00d5f9f86dc2
mathieuabbal <mathieuabbal@hotmail.com>
2026-02-02 18:38:44 +0100

    lazy loading photos, scroll-to-top, icones PWA famille, tracking global

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { BottomNavComponent } from './layout/bottom-nav/bottom-nav.component';
 import { TopBarComponent } from './layout/top-bar/top-bar.component';
 import { auth } from './firebase';
 import { onAuthStateChanged } from 'firebase/auth';
+import { LocationService } from './services/location.service';
 
 @Component({
     selector: 'app-root',
@@ -17,9 +18,16 @@ export class AppComponent implements OnInit {
   title = 'FamilyApp';
   isAuth = false;
 
+  constructor(private locationService: LocationService) {}
+
   ngOnInit() {
     onAuthStateChanged(auth, user => {
       this.isAuth = !!user;
+      if (user) {
+        this.locationService.startTracking();
+      } else {
+        this.locationService.stopTracking();
+      }
     });
   }
 }
